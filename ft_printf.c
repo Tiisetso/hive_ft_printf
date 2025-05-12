d9af9b50318792cce0a887eddc180457e98568b7
Tiisetso Murray <timurray@student.hive.fi>
2025-05-12 09:58:35 +0300

    TODOs

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,30 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 16:06:31 by timurray          #+#    #+#             */
-/*   Updated: 2025/05/08 16:06:32 by timurray         ###   ########.fr       */
+/*   Updated: 2025/05/12 09:57:59 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_printf(const char *, ...)
+{
+	
+}
+
+/* 
+TODO • Do not implement the original printf()’s buffer management.
+TODO • Your function has to handle the following conversions: cspdiuxX%
+TODO • Your implementation will be evaluated against the behavior of the original printf().
+TODO • You must use the command ar to create your library.
+TODO • The use of the libtool command is strictly forbidden.
+TODO • libftprintf.a must be created at the root of your repository.
+TODO • You have to implement the following conversions:
+TODO • %c Prints a single character.
+TODO • %s Prints a string (as defined by the common C convention).
+TODO • %p The void * pointer argument has to be printed in hexadecimal format.
+TODO • %d Prints a decimal (base 10) number.
+TODO • %i Prints an integer in base 10.
+TODO • %u Prints an unsigned decimal (base 10) number.
+TODO • %x Prints a number in hexadecimal (base 16) lowercase format.
+TODO • %X Prints a number in hexadecimal (base 16) uppercase format.
+TODO • %% Prints a percent sign 
+*/
