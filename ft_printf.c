44df8fb5e5215ceda1a69d8d14a156d2cb4db93e
Tiisetso Murray <timurray@student.hive.fi>
2025-06-06 16:45:03 +0300

    Simpler execution. If statements. Trying out guard clauses. Expand functionality.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 16:06:31 by timurray          #+#    #+#             */
-/*   Updated: 2025/06/06 14:35:25 by timurray         ###   ########.fr       */
+/*   Updated: 2025/06/06 16:39:23 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 #include <stdio.h> //TODO Remove
 #include <unistd.h>
 
-int char_handler(char c)
+int char_handler(int c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
 int str_handler(char *s)
@@ -26,25 +26,37 @@ int str_handler(char *s)
 	count = 0;
 	while(*s)
 		count += write(1, s++, 1);
-	return count;
+	return (count);
 }
 
+int func_select(const char key, va_list arg)
+{
+	size_t length;
+
+	length = 0;
+	if(key == 'c')
+		return (char_handler(va_arg(arg, int)));
+	if(key == '%')
+		return (char_handler('%'));
+	if(key == 's')
+		return (str_handler(va_arg(arg, char *)));
+	return (length);
+}
 
 int ft_printf(const char *s, ...)
 {
 	va_list	args;
-	size_t	i;
 	size_t length;
 
-	i = 0;
 	length = 0;	
 	va_start(args, s);
-	while(*(s + i))
+	while(*s)
 	{
-		if (s[i] == '%')
-		{
-		}
-		i++;
+		if (*s == '%')
+			length += func_select(*++s, args);
+		else
+			length += write(1, s, 1);
+		s++;
 	}
 	va_end(args);
 	return(length);
@@ -52,8 +64,8 @@ int ft_printf(const char *s, ...)
 
 int main (void)
 {
-	int count = ft_printf("This is a number: %d and this is a string: %s", 55,"test string");
-	printf("\nchar count: %i", count);
+	int count = ft_printf("This is a number: %c and this is a string: %s", 'c',"test string");
+	printf("\n\nchar count: %i", count);
 	return (0);
 }
 
@@ -81,4 +93,5 @@ TODO • %x Prints a number in hexadecimal (base 16) lowercase format.
 TODO • %X Prints a number in hexadecimal (base 16) uppercase format.
 
 TODO • %% Prints a percent sign
+TODO What about multiple %%% and percent at the end?
 */
